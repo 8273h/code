34c318b0ab57d43f90318aebe2873ccb54064453
salman <salman@123>
2023-02-17 20:01:31 +0530

    third commit for me

--- hello.java
+++ hello.java
@@ -11,3 +11,10 @@ public class hello {
         System.out.println(2 * 5);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello World");
+    }
+}
